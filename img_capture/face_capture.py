bf96b2cfad3764e383181c5a36a2237ab6ef37b4
Vijay Singh <vijjus@berkeley.edu>
2020-01-18 19:43:35 -0800

    Changes to face capture container

--- img_capture/face_capture.py
+++ img_capture/face_capture.py
@@ -1,14 +1,10 @@
-import numpy as np
-import cv2 as cv
 import paho.mqtt.client as mqtt
-
-print(cv.__version__)
+import cv2 as cv
 
 # Open Issues:
-# a. Identify local container by name
 # b. Encode messages for transmission
 # c. Ensure only faces are captured
-LOCAL_MQTT_HOST="localhost"
+LOCAL_MQTT_HOST="mqtt_broker"
 LOCAL_MQTT_PORT=1883
 LOCAL_MQTT_TOPIC="face_images"
 
@@ -23,8 +19,8 @@ mqttclient.on_connect = on_connect_local
 mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
 mqttclient.on_publish = on_publish
 
-model_file="/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml"
-# 1 should correspond to /dev/video1 , your USB camera. The 0 is reserved for the TX2 onboard camera
+model_file="/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml"
+# 1 should correspond to /dev/video1, your USB camera. The 0 is reserved for the TX2 onboard camera
 cap = cv.VideoCapture(1)
 face_cascade = cv.CascadeClassifier(model_file)
 
@@ -35,15 +31,18 @@ while True:
     ret, frame = cap.read()
 
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    img = cv.imshow('frame', gray)
     for (x,y,w,h) in faces:
-        cv.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
-        cv.imshow('img',frame)
+        #cv.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        crop_face = gray[y:y+h, x:x+w]
+        cv.imshow('img', crop_face)
         count += 1
+        rv, buf = cv.imencode('.png', crop_face)
+        print("{}".format(rv))
         mqttclient.publish(LOCAL_MQTT_TOPIC, count)
+
     cv.waitKey(0)
 
 cv.destroyAllWindows()
